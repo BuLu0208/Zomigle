18c3cb5f411a0d95cc3ede4bc4615bf9c310edb8
BuLu0208 <1169321425@qq.com>
2025-02-17 18:21:06 +0800

    修复

--- Zomigle/trollstore_helper.m
+++ Zomigle/trollstore_helper.m
@@ -4,15 +4,25 @@
 #import <sys/stat.h>
 
 BOOL isTrollStore(void) {
-    // 检查 TrollStore 特定文件
-    return (access("/var/mobile/Library/Preferences", R_OK) == 0) &&
-           (access("/var/mobile/Library/Preferences/com.apple.NanoRegistry.plist", R_OK) == 0);
+    NSString *executablePath = [[NSBundle mainBundle] executablePath];
+    NSString *trollStoreMark = [executablePath stringByAppendingString:@".installed_trollstore"];
+    return [[NSFileManager defaultManager] fileExistsAtPath:trollStoreMark];
 }
 
 void setTrollStoreEnvironment(void) {
-    // 设置环境变量
-    setenv("TROLLSTORE", "1", 1);
+    NSString *executablePath = [[NSBundle mainBundle] executablePath];
+    NSString *trollStoreMark = [executablePath stringByAppendingString:@".installed_trollstore"];
+    [[NSFileManager defaultManager] createFileAtPath:trollStoreMark contents:nil attributes:nil];
+    
     // 设置文件权限
-    chmod("/var/mobile/Library/Preferences", 0755);
-    chmod("/var/mobile/Library/Preferences/com.apple.NanoRegistry.plist", 0644);
+    NSString *prefsPath = @"/var/mobile/Library/Preferences";
+    NSString *nanoRegistryPath = @"/var/mobile/Library/Preferences/com.apple.NanoRegistry.plist";
+    
+    // 创建符号链接
+    NSString *bundlePrefsPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Library/Preferences"];
+    [[NSFileManager defaultManager] createDirectoryAtPath:bundlePrefsPath withIntermediateDirectories:YES attributes:nil error:nil];
+    symlink([prefsPath UTF8String], [[bundlePrefsPath stringByAppendingPathComponent:@"mobile"] UTF8String]);
+    
+    chmod([prefsPath UTF8String], 0755);
+    chmod([nanoRegistryPath UTF8String], 0644);
 } 
