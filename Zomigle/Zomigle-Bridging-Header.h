b55171df934c7394c991b1796b444ce7a99360d0
BuLu0208 <1169321425@qq.com>
2025-02-17 18:18:35 +0800

    修复

--- Zomigle/Zomigle-Bridging-Header.h
+++ Zomigle/Zomigle-Bridging-Header.h
@@ -8,8 +8,10 @@
 #ifndef Zomigle_Bridging_Header_h
 #define Zomigle_Bridging_Header_h
 
+#import <Foundation/Foundation.h>
+
 void respring(void);
-bool isTrollStore(void);
+BOOL isTrollStore(void);
 void setTrollStoreEnvironment(void);
 
 #endif /* Zomigle_Bridging_Header_h */
--- Zomigle/trollstore_helper.m
+++ Zomigle/trollstore_helper.m
@@ -1,13 +1,15 @@
 #import <Foundation/Foundation.h>
 #import <dlfcn.h>
+#import <unistd.h>
+#import <sys/stat.h>
 
-bool isTrollStore() {
+BOOL isTrollStore(void) {
     // 检查 TrollStore 特定文件
     return (access("/var/mobile/Library/Preferences", R_OK) == 0) &&
            (access("/var/mobile/Library/Preferences/com.apple.NanoRegistry.plist", R_OK) == 0);
 }
 
-void setTrollStoreEnvironment() {
+void setTrollStoreEnvironment(void) {
     // 设置环境变量
     setenv("TROLLSTORE", "1", 1);
     // 设置文件权限
